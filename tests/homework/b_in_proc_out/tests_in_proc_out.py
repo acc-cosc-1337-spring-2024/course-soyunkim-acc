ee162fbf5054e691e999964abd39238a0e54870e
soyunkim-acc <157082395+soyunkim-acc@users.noreply.github.com>
2024-01-26 03:36:24 +0000

    Hw 1 input output arithmetic operations

--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -4,7 +4,8 @@ import unittest
 The file at /src/homework/b_in_proc_out/output has 
 the function get_number.
 '''
-from src.homework.b_in_proc_out.output import get_number
+
+from src.homework.b_in_proc_out.output import get_number, multiply_numbers
 
 class Test_Config(unittest.TestCase):
 
@@ -15,4 +16,10 @@ class Test_Config(unittest.TestCase):
     def test_get_number_2(self):
         #test that the function get_number returns 2
         self.assertEqual(2, get_number(2))
+
+    def test_multiply_numbers_1(self):
+         self.assertEqual(multiply_numbers(5,5), 25)
+
+    def test_multiply_numbers_2(self):
+        self.assertEqual(multiply_numbers(10, 10), 100)
 
