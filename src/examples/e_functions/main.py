a6d9df0ad4f12ad827c86ce1b9370b1ff7e6be9f
soyunkim-acc <157082395+soyunkim-acc@users.noreply.github.com>
2024-02-20 06:39:21 +0000

    variables & parameter

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,1 +1,18 @@
 #main program
+import void_functions, value_return_functions
+
+void_functions.say_hello(Python)
+
+void_functions.use_global_variable()
+
+void_functions.change_global_variable()
+
+void_functions.use_global_variables_w_param(10)
+print("Global: ", void_functions.val2)
+
+num1 = 5
+num2 = 10
+result = value_return_functions.get_product(num1, num2)
+
+print(result)
+#involves 4 variables. num1 & num 2 and num1 & num2 in get_product()
--- src/examples/e_functions/value_return_functions.py
+++ src/examples/e_functions/value_return_functions.py
@@ -1,2 +1,5 @@
 def test_config():
     return True
+
+def get_product(num1, num2):
+    return num1 * num2
