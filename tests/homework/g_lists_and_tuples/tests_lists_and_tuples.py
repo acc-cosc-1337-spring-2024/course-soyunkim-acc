c50cf16f5c845eb9a76447aeadc61dc80524da4b
soyunkim-acc <157082395+soyunkim-acc@users.noreply.github.com>
2024-04-04 22:57:34 +0000

    list w/ diff data types

--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,1 +1,12 @@
-#
+def test_list_supports_different_data_type_elements(self):
+    list1 = [1, 5.5, 'a']
+
+    self.assertEqual(list1[0], 1)
+    self.assertEqual(list1[1], 5.5)   
+    self.assertEqual(list1[2], 'a')
+
+def test_list_supports_list_as_elements(self):
+    sub_list = [5, 9, 2]
+    list1 = [sub_list]   #makes sub_list an element of list1
+
+    self.assertEqual(list1[0], [5, 9, 2])
